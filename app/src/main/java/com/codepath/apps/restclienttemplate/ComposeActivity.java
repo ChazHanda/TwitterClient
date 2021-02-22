80e4eeecac450f13e26e1d38d68a8dfe0e63dca6
ChazHanda <ChazHanda@gmail.com>
2021-02-21 18:00:03 -0800

    Added text counter Changed colors for the tweet screen

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,12 +3,47 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ComposeActivity extends AppCompatActivity {
+
+    public static final int MAX_TWEET_LENGTH = 140;
+    public static final String TAG = "ComposeActivity";
+
+    EditText etCompose;
+    Button btnTweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet = findViewById(R.id.btnTweet);
+
+        // Set click listener on button
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tweetContext = etCompose.getText().toString();
+                if (tweetContext.isEmpty()) {
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "The tweet is empty: " + tweetContext);
+                    return;
+                }
+                if (tweetContext.length() > MAX_TWEET_LENGTH) {
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long", Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "The tweet is too long: " + tweetContext);
+                    return;
+                }
+                //Toast.makeText(ComposeActivity.this, tweetContext, Toast.LENGTH_LONG).show();
+                Log.i(TAG, "Sending the tweet: " + tweetContext);
+                // Make API call to Twitter to publish the tweet
+            }
+        });
     }
 }
